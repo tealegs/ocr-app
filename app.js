a9c0748c3070530bfe28d55e9f7f84438c4b27b9
Tom Legnard <tomlegnard@toms-mbp.home>
2020-06-18 00:34:49 -0400

    initial commit, OCR is working.

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+const fs = require("fs");
+const multer = require('multer');
+const {createWorker} = require('tesseract.js');
+const worker = createWorker();
+
+const storage = multer.diskStorage({
+    destination: (req,file,cb) => {
+        cb(null, "./uploads")
+    },
+    filename: (req,file,cb) => {
+        cb(null, file.originalname);
+    } 
+});
+
+const upload = multer({storage: storage}).single('avatar');
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+//ROUTES
+app.get('/', (req,res) => {
+    res.render('index')
+})
+
+app.post("/upload", (req,res) => {
+    upload(req,res,err => {
+        fs.readFile(`./uploads/${req.file.originalname}`, (err, image) => {
+            if (err) return console.log("This is your error", err);
+            ////old code from tutorial
+            // worker
+            //     .recognize(data, "eng", { tessjs_create_pdf: "1" })
+            //     .progress(progress => {console.log(progress);})
+            //     .then(result => {
+            //         res.send(result.text);
+            //         //res.redirect('/download')
+            //     })
+            //     .finally(() => worker.terminate());
+            
+            ////new code found from docs
+            (async () => {
+                const worker = createWorker();
+                await worker.load();
+                await worker.loadLanguage('eng');
+                await worker.initialize('eng');
+                const { data: { text } } = await worker.recognize(image);
+                console.log(text);
+                const { data } = await worker.getPDF('Tesseract OCR Result');
+                fs.writeFileSync('tesseract-ocr-result.pdf', Buffer.from(data));
+                //TODO - setup download of pdf
+                console.log('Generate PDF: tesseract-ocr-result.pdf');
+                await worker.terminate();
+              })();
+        });
+    });
+});
+
+app.get('/download', (req,res) => {
+    const file = `${__dirname}/tesseract.js-ocr-result.pdf`
+})
+
+//start up server
+const PORT = 5000 || process.env.PORT;
+app.listen(PORT, () => console.log(`Hey I'm running on port ${PORT}`))
+
+//APP THAT READS TEXT ON A SCREEN TO DISABLED PEOPLE (DYSLEXIC, BLIND, ETC)
